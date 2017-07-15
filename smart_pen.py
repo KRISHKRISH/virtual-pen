8208f66ebcc845fc396a92b9e02d1f998150ba44
KRISHKRISH <KRISHKRISH@users.noreply.github.com>
2017-07-14 21:10:08 -0700

    Add files via upload

---
+++ smart_pen.py
@@ -0,0 +1,86 @@
+def background(cam):
+  bck_gnd = np.zeros((480,640,3), np.float32)*0
+  for i in range(0,100):
+    ret_val, img = cam.read()
+    #print np.amax(img)
+    bck_gnd=bck_gnd+img;
+  bck_gnd=bck_gnd/100;
+  bck_gnd=bck_gnd.astype(np.uint8) 
+  return(bck_gnd)
+
+
+def caliberate(cam,bck_gnd):
+  src=[];
+  dst=[[0,0],[640,0],[640,480],[0,480]];
+  while True :
+    ret_val, img = cam.read()
+    img=cv2.absdiff(img,bck_gnd)
+    mask = cv2.inRange(img, lower, upper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    cv2.imshow('my webcam', mask)
+    if cv2.waitKey(1) == 8:
+      M = cv2.moments(mask)
+      if(M["m10"]>0):
+        print len(src)+1
+        center = [int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])]
+        src.append(center)
+        if(len(src)==4):
+          break;
+    if cv2.waitKey(1) == 27:
+    		break;
+  src=np.float32(src);
+  dst=np.float32(dst);
+  m = cv2.getPerspectiveTransform(src, dst)
+  cv2.destroyAllWindows()   
+  return(m)
+def track(p1,p2,image,cam,bck_gnd):
+  ret_val, img = cam.read()
+  img=cv2.absdiff(img,bck_gnd)
+  mask = cv2.inRange(img, lower, upper)
+  mask = cv2.erode(mask, None, iterations=2)
+  mask = cv2.dilate(mask, None, iterations=2)
+  cv2.imshow('tracking', mask)
+  cv2.imshow('display',image)
+  if cv2.waitKey(1) == 27: 
+     print "exiting......"
+     exit();
+  M = cv2.moments(mask)
+  if M["m00"]>0 :
+     center = [int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])]
+     p2=p1;
+     p1 = np.array([center], dtype=np.float32)
+     result = cv2.perspectiveTransform(p1[None,:], m)
+     p1=result[0][0]
+     if not all(tuple(p2)):
+      return(p1,p2,image)
+     print tuple(p1)
+     print tuple(p2)
+     cv2.line(image,tuple(p2), tuple(p1), (0, 0, 255),2)
+  else :
+     p1=np.array([None,None,None]);
+     p2=np.array([None,None,None]);
+  return(p1,p2,image)
+
+
+
+# main
+import cv2
+import numpy as np
+cam = cv2.VideoCapture(0)
+lower = np.array([2,2,2])
+upper = np.array([255,255,255])
+image = np.ones((480,640,3), np.uint8)
+image = image*255
+p1=np.array([None,None,None]);
+p2=np.array([None,None,None]);
+#background 
+print "background"
+bck_gnd=background(cam);
+#claiberation
+print "caliberating"
+m=caliberate(cam,bck_gnd);
+print "tarcking"
+while True :
+  p1,p2,image=track(p1,p2,image,cam,bck_gnd);
+cv2.destroyAllWindows()
